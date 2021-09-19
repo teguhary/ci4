c4ae79c9068b53adddb109c49d7b85b10f73e83e
Teguh Ary <teguhary36@gmail.com>
2021-09-20 01:54:21 +0700

    add caption product slider

--- app/Views/home/discount.php
+++ app/Views/home/discount.php
@@ -1,3 +1,3 @@
-<div class="d-flex justify-content-center " style="margin-top: 100px; margin-bottom: 100px;">
+<div class="d-flex justify-content-center" style="margin-top: 130px; margin-bottom: 130px;">
     <img src="img/discount.png" style="width: 1220px ;" >
 </div>
--- app/Views/home/productSlider.php
+++ app/Views/home/productSlider.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="m-5">
     <div id="carouselProduct" class="carousel slide" data-bs-ride="carousel">
     <ol class="carousel-indicators">
         <li data-bs-target="#carouselProduct" data-bs-slide-to="0" class="active"></li>
@@ -7,21 +7,38 @@
     </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="img/imageSlider.png"  class="d-block w-100" alt="...">
+                <img src="img/product.png"  class="d-block" style="width: 30%;">
+                <div class="carousel-caption" style="text-align: left; color: black; margin-bottom: 20px; margin-left: 350px;">
+                    <div class="row">
+                        <div class="col">
+                            <h1 style="font-weight: bold;">Yasa Facial Wash</h1>
+                        </div>
+                        <div class="col-4">
+                            <div class="alert text-center" style="background-color: #F7A590; width: 100px; height: 50px; color: white; padding-bottom: 20px;">
+                                SALE
+                            </div>
+                        </div>
+                    </div>
+                        <div class="col">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.    Mauris sit amet maximus nibh. Mauris elementum erat varius volutpat tristique. Ut non fringilla sapien, quis tempor purus.     Sed nec pellentesque massa. Aliquam erat volutpat. Pellentesque suscipit porttitor luctus. Aliquam quis elit risus.</p>
+                            <h3 class="mt-5">RP 90.000</h3>
+                        </div>
+                        <button type="button" class=" btn  border-2 my-5 py-1" style=" border-color: grey;">SHOP NOW</button>
+                </div>
             </div>
             <div class="carousel-item">
-                <img src="img/imageSlider.png" class="d-block w-100" alt="...">
+                <img src="img/product.png" class="d-block" style="width: 30%;">
             </div>
             <div class="carousel-item">
-                <img src="img/imageSlider.png" class="d-block w-100" alt="...">
+                <img src="img/product.png" class="d-block" style="width: 30%;">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselProduct" data-bs-slide="prev">
-            <img src="img/btnPrev.png" style="width: 35%;">
+            <img src="img/btnPrev.png" style="width: 35%; margin-left: -40px;">
             <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselProduct" data-bs-slide="next">
-            <img src="img/btnNext.png" style="width: 35%;">
+            <img src="img/btnNext.png" style="width: 35%; margin-right: -50px;">
             <span class="visually-hidden">Next</span>
         </button>
     </div>
