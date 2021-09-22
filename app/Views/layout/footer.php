407dc2f85c40fdb083b13fc11a0af1dc6759a474
Teguh Ary <teguhary36@gmail.com>
2021-09-22 22:53:23 +0700

    add footer

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,1 +1,53 @@
 <hr>
+<div class="row m-5">
+    <div class="col p-5">
+        <img src="img/favicon.ico">
+    </div>
+    <div class="col">
+        <ol style="list-style: none;">
+            <li class="mt-5">
+                <hr style="transform: rotate(90deg); margin-top: 70px;">
+            </li>
+        </ol>
+        
+    </div>
+    <div class="col">
+        <ol style="list-style: none;">
+            <li class="mb-2">
+                <h5>Site Map</h5>
+            </li>
+            <li class="mb-2">Home</li>
+            <li class="mb-2">Shop</li>
+            <li class="mb-2">Collection</li>
+            <li class="mb-2">Contact Us</li>
+        </ol>
+    </div>
+    <div class="col">
+        <ol style="list-style: none;">
+            <li class="mb-2">
+                <h5>Contact Us</h5>
+            </li>
+            <li class="mb-2">+62 82342133</li>
+            <li class="mb-2">Loremipsum@gmail.com</li>
+        </ol>
+    </div>
+    <div class="col">
+        <ol style="list-style: none;">
+            <li class="mb-2">
+                <h5>Visit Us</h5>
+            </li>
+            <li class="mb-2">Jalan Karawitan No.24a Pertigaan 
+            Karawitan-Selontongan SMA 8 Bandung ( Buah Batu )</li>
+        </ol>
+    </div>
+    <div class="col">
+        <ol style="list-style: none;">
+            <li class="mb-2">
+                <h5>Follow Us</h5>
+            </li>
+            <li class="mb-2">
+                <img src="img/favicon.ico">
+            </li>
+        </ol>
+    </div>
+</div>
